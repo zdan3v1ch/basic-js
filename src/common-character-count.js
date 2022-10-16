d5c61a6ba19fa67ae45c69dfbd6ed6fd694d0e16
Anatoli_Z <anatoli.zdanevitch@gmail.com>
2022-10-16 23:42:43 +0300

    feat: add some solutions

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,4 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {
+  NotImplementedError
+} = require('../extensions/index.js');
 
 /**
  * Given two strings, find the number of common characters between them.
@@ -11,9 +13,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+
+  let count = 0;
+  let arr1 = s1.split('')
+  let arr2 = s2.split('')
+
+  for (let i = 0; i < arr1.length; i++) {
+    let sovp = arr2.indexOf(arr1[i]);
+
+    if (sovp !== -1) {
+      arr2.splice(sovp, 1);
+      count++;
+    }
+  }
+
+  return count;
+
 }
 
 module.exports = {
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,4 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {
+  NotImplementedError
+} = require('../extensions/index.js');
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
@@ -20,13 +22,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  code(rev, shft, msg, key) {
+    if (!msg || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let ascII = 65;
+    let count = 0;
+    let msgArr = msg.toUpperCase().split('');
+    let keyArr = key.toUpperCase().split('').map(el => el.charCodeAt(0) - ascII);
+    let result = [];
+
+    for (let i = 0; i < msgArr.length; i++) {
+      let charPos = msgArr[i].charCodeAt(0);
+      if (charPos >= 65 && charPos < 91) {
+        result.push(String.fromCharCode(ascII + (charPos - ascII + rev * keyArr[count % keyArr.length] + shft) % 26));
+        count++;
+      } else {
+        result.push(msgArr[i]);
+      }
+    }
+
+    return this.direct ? result.join('') : res.reverse().join('');
+  }
+
+  encrypt(msg, key) {
+    let rev = 1;
+    let shft = 0;
+    return this.code(rev, shft, msg, key);
+  }
+
+  decrypt(msg, key) {
+    let rev = -1;
+    let shft = 26;
+    return this.code(rev, shft, msg, key);
   }
 }
 
